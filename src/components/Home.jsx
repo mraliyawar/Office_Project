1f07bd6c4686a698176633c997ac9cdc89eb40d0
mraliyawar <125953255+mraliyawar@users.noreply.github.com>
2024-02-27 18:17:38 +0530

    27Feb work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,47 +36,48 @@ const Home = () => {
       Getuser();
     }
   });
-  // const Connect = () => {
-  //   window.addEventListener("load", async () => {
-  //     accounts = await ethereum.request({ method: "eth_requestAccounts" });
-  //     userAddr = accounts[0];
-  //     window.web3 = new Web3(Web3.givenProvider);
-  //     contract = new web3.eth.Contract(abi, contractAddress);
-  //     usdtcontract = new web3.eth.Contract(usdtabi, usdtcontractAddress);
+  const Connect = () => {
+    window.addEventListener("load", async () => {
+      accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      userAddr = accounts[0];
+      window.web3 = new Web3(Web3.givenProvider);
+      contract = new web3.eth.Contract(abi, contractAddress);
+      usdtcontract = new web3.eth.Contract(usdtabi, usdtcontractAddress);
 
-  //     if (accounts) {
-  //       Getuser();
-  //     } else {
-  //       window.location = "register.html";
-  //     }
-  //   });
-  // };
-  const getWeb3State = async () => {
-    let [contractInstance, selectedAccount, chainId] = [null, null, null, null];
-    try {
-      if (!window.ethereum) {
-        throw new Error("Metamask is not installed");
+      if (accounts) {
+        Getuser();
+      } else {
+        window.location = "register.html";
       }
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      let chainIdHex = await window.ethereum.request({
-        method: "eth_chainId",
-      });
-      chainId = parseInt(chainIdHex, 16);
-      selectedAccount = accounts[0];
-      //read operation
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      //write operation
-      const signer = await provider.getSigner();
-      const contractAddress = "0xCCC15B5CCAF92d34f3A99c2270920D3Fcf42c290";
-      contractInstance = new ethers.Contract(contractAddress, abi, signer);
-      return { contractInstance, chainId, selectedAccount };
-    } catch (error) {
-      console.error("Not able to get the web3states", error.message);
-      throw error;
-    }
+    });
   };
+
+  // const getWeb3State = async () => {
+  //   let [contractInstance, selectedAccount, chainId] = [null, null, null, null];
+  //   try {
+  //     if (!window.ethereum) {
+  //       throw new Error("Metamask is not installed");
+  //     }
+  //     const accounts = await window.ethereum.request({
+  //       method: "eth_requestAccounts",
+  //     });
+  //     let chainIdHex = await window.ethereum.request({
+  //       method: "eth_chainId",
+  //     });
+  //     chainId = parseInt(chainIdHex, 16);
+  //     selectedAccount = accounts[0];
+  //     //read operation
+  //     const provider = new ethers.BrowserProvider(window.ethereum);
+  //     //write operation
+  //     const signer = await provider.getSigner();
+  //     const contractAddress = "0xCCC15B5CCAF92d34f3A99c2270920D3Fcf42c290";
+  //     contractInstance = new ethers.Contract(contractAddress, abi, signer);
+  //     return { contractInstance, chainId, selectedAccount };
+  //   } catch (error) {
+  //     console.error("Not able to get the web3states", error.message);
+  //     throw error;
+  //   }
+  // };
 
   async function Getuser() {
     let userId = getQueryVariable("id");
@@ -466,7 +467,6 @@ const Home = () => {
       <Header getWeb3State={getWeb3State} />
       <HeroSec />
       <About />
-
       <Vision />
       <Carousel />
       <Features />
